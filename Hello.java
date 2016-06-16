bae8d2468f43ad2c38bd762a367053f4c23652cd
sky <1005672068@qq.com>
2016-06-16 09:40:45 +0800

    change hello.java

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+import java.utils.*;
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
@@ -8,5 +9,13 @@ public class Hello{
 		System.out.println("活着的时候，就需要有压力，要不活着跟死人有什么区别呢？");
 		
 		System.out.println("正在努力拼命中，升级版本有压力……");
+		
+		//打印随机数
+		random();
+	}
+	
+	public static void random(){
+		Random random = new Random();
+		System.out.println("random number is :" + random.nextInt());
 	}
 }
