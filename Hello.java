9950bc58a3aac4fcb029f79e9bc35612bca09299
sky <1005672068@qq.com>
2016-06-16 08:58:34 +0800

    add a new line

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		
+		Systeml.out.println("今天我想休息，但是不行，压力山大");
+		// .....................
 	}
 }
